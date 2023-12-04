6d42a8bc44a3ad4a3524f263de9804f0187b7eaf
TSUBASA OHI <tsubasaaa.1234@gmail.com>
2023-12-04 21:04:42 +0900

    回数制限を実装

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void){
-    int ans = rand() % 10;
+    srand((unsigned)time(NULL));
+    int ans = rand() % 31;
     int num;
+    int max_try = 5;
+    int remain = max_try;
     do {
-        printf("1桁の数値を入力してください：");
+        printf("30までの数値を入力してください：");
         scanf("%d", &num);
         if (num < ans){
             printf("誤りです。もっと大きい数値です\n");
         } else if (num > ans){
             printf("誤りです。もっと小さい数値です\n");
         } 
-    } while (ans != num);
-    printf("正解です！\n");
+        remain --;
+    } while (ans != num && remain > 0);
+    if (ans == num){
+        printf("正解です！\n");
+    } else {
+        printf("回数制限を超えました\n");
+    }
     return 0;
 }
